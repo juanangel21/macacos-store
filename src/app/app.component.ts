b4415770679818de510dca1157120f296df2834f
Juan Angel Ibarra <juan08angel@gmail.com>
2025-04-06 23:49:54 -0300

    Creacion de Main
    Se crea el main component para el landing, agregando ademas imagen de fondo personalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MainComponent} from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, RouterOutlet],
+  imports: [HeaderComponent, MainComponent, CommonModule, RouterOutlet],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
@@ -16,4 +18,4 @@ import { HeaderComponent } from './components/header/header.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }
