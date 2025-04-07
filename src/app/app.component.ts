bf8721c83fff0b7c61a25397beec2f93db34174f
junlugo <152571128+junlugo@users.noreply.github.com>
2025-04-06 23:37:41 -0300

    subida del Header
    Se sube los archivos del encabezado de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [HeaderComponent, CommonModule, RouterOutlet],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
