62ec3a914907ea87712b38e7453fc09a4f054a88
sisifo9876 <claudio.areco@hotmail.com>
2025-04-24 20:11:51 -0300

    mis cambios profe

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,32 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  navLinks = ['Inicio', 'Nosotros', 'Tienda', 'Ayuda'];
+  navLinks = [
+    { name: 'Inicio', path: '/' },
+    { name: 'Nosotros', path: '/nosotros' },
+    { name: 'Tienda', path: '/productos' },
+    { name: 'Ayuda', path: '/ayuda' }
+  ];
   showCartAnimation = false;
+  totalItems = 0;
+
+  constructor(private cartService: CartService) {
+    // Suscribirse al observable de cambios del carrito
+    this.cartService.getTotalItems$().subscribe(count => {
+      this.totalItems = count;
+    });
+  }
 
   toggleMenu() {
     this.showCartAnimation = true;
